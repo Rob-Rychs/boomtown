b353f82a854e03a47ac7003382b1c2d739359df7
Rob-Rychs <Robrychel@gmail.com>
2017-10-19 14:54:28 -0700

    added server/graphql/resolvers.js

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -1,5 +1,51 @@
 import fetch from 'node-fetch';
 const resolveFunctions = {
-  // Resolvers go here...
+  Query: {
+    items() {
+      return fetch('http://localhost:3001/items')
+        .then(response => response.json())
+        .catch(errors => console.log(srrors));
+    },
+    users() {
+      return fetch('http://localhost:3001/users')
+        .then(response => response.jspn())
+        .catch(errors => console.log(errors));
+    },
+    item(root, { id }) {
+      return fetch(`http://localhost:3001/items/${id}`)
+        .then(response => response.json())
+        .catch(erors => console.log(errors));
+    },
+    user(root, { id }) {
+      return fetch(`http://localhost:3001/users/${id}`)
+        .then(response => response.json())
+        .catch(errors => console.log(errors));
+    } 
+  },
+  Item: {
+    itemowner(item) {
+      return fetch(`http://localhost:3001/users/${item.itemowner}`)
+      .then(response => response.json())
+      .catch(errors => console.log(errors));
+    },
+    borrower(item) {
+      if(!item.borrower) return null
+      return fetch(`http://localhost:3001/users/${item.borrower}`)
+      .then(response => response.json())
+      .catch(errors => console.log(errors));
+    }
+  },
+  User: {
+    async owneditems(user) {
+      const response = await fetch(`http://localhost:3001/items/?itemowner=${user.id}`)
+      const items = await response.json()
+      return items
+    },
+    async borroweditems(user) {
+      const response = await fetch(`http://localhost:3001/items/?borrower=${user.id}`)
+      const items = await response.json()
+      return items
+    }
+  }
 };
 export default resolveFunctions;
